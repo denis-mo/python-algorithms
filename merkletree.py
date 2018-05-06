5820b63dad5a0e305fd962187f0774e50d1c618a
Deniss Mokijevski <Deniss.Mokijevski@relax-gaming.com>
2018-05-06 18:42:51 +0300

    using namedtuples for path traversing, verify is static method, new tests

--- merkletree.py
+++ merkletree.py
@@ -1,87 +1,104 @@
 import hashlib
 from functools import reduce
+from enum import Enum
+from collections import namedtuple
 
+class Dir(Enum):
+    left = 1,
+    right = 2
+    
+Step = namedtuple('Step', ['dir', 'hash'])
+Path = namedtuple('Path', ['steps', 'leaf_hash'])
 
-class Node(object):
-    def __init__(self, data, side, neighbor=None, left_leaf=None, right_leaf=None):
+class _Node(object):
+    def __init__(self, data, dir, left_leaf=None, right_leaf=None):
         self.data = data
-        self.side = side
+        self.dir = dir
         self.parent = None
-        self.neighbor = neighbor
-        if left_leaf is not None:
-            left_leaf.parent = self
-            right_leaf.parent = self
-        if neighbor is not None:
-            neighbor.neighbor = self
+        self.neighbor = None
 
 
 class MerkleTree(object):
     def __init__(self, values=[]):
-        self.root_hash = ""
+        self.root_hash = None
         self.leaves = []
 
+        #append one more element to make tree even
         if len(values) % 2 == 1:
             values.append(values[-1])
 
-        for past_i, value in enumerate(values):
-            if past_i % 2 == 0:
-                self.leaves.append(Node(sha(value), 1))
+        #create list of leaves which will be used for getting a path
+        for i, value in enumerate(values):
+            if i % 2 == 0:
+                self.__append_left(self.leaves, _Node(sha(value), Dir.right))
             else:
-                self.leaves.append(Node(sha(value), 0, self.leaves[past_i - 1]))
+                self.__append_right(self.leaves, _Node(sha(value), Dir.left))
 
+        #iterate over every level of the tree
         past_leaves = list(self.leaves)
-
         while len(past_leaves) > 1:
-            current_nodes = []
+            cur_nodes = []
+            #walk 2 steps over previous number of elements, reducing by 2 on every level	
             for i, past_i in enumerate(range(0, len(past_leaves), 2)):
                 left_leaf = past_leaves[past_i]
                 right_leaf = past_leaves[past_i + 1]
                 sha_hash = sha(left_leaf.data, right_leaf.data)
+                node = None
                 if i % 2 == 0:
-                    node = Node(sha_hash, 1, None, left_leaf, right_leaf)
+                    node = _Node(sha_hash, Dir.right)
+                    self.__append_left(cur_nodes, node)
                 else:
-                    node = Node(sha_hash, 0, current_nodes[i - 1], left_leaf, right_leaf)
-                current_nodes.append(node)
+                    node = _Node(sha_hash, Dir.left)
+                    self.__append_right(cur_nodes, node)
+                left_leaf.parent = node
+                right_leaf.parent = node
 
-            # appending new node if number of nodes is even
-            if len(current_nodes) != 1 and len(current_nodes) % 2 == 1:
-                last_node = current_nodes[-1]
-                current_nodes.append(Node(last_node.data, 0, last_node))
+            # appending new node if number of nodes is odd
+            if len(cur_nodes) != 1 and len(cur_nodes) % 2 == 1:
+                last_node = cur_nodes[-1]
+                self.__append_right(cur_nodes, _Node(last_node.data, Dir.left))
 
-            if len(current_nodes) == 1:
-                self.root_hash = current_nodes[0].data
+            # we concluded our hashes into final root_hash
+            if len(cur_nodes) == 1:
+                self.root_hash = cur_nodes[0].data
 
-            past_leaves = list(current_nodes)
-
-    def validate(self, item="", path=[]):
-        path = list(path)
-        path.insert(0, sha(item))
-        root_hash = reduce(self.merklestep, path)
-        return True if root_hash == self.root_hash else False
+            past_leaves = list(cur_nodes)         
 
     def get_path(self, item):
-        sha_hash = sha(item)
-        for i, leaf in enumerate(self.leaves):
-            if leaf.data == sha_hash:
-                current_node = leaf
-                path = []
+        item_hash = sha(item)
+        for leaf in self.leaves:
+            if leaf.data == item_hash:
+                cur_node = leaf
+                steps = []
 
-                while current_node.parent is not None:
-                    neighbor = current_node.neighbor
-                    path.append((neighbor.side, neighbor.data))
-                    current_node = current_node.parent
-                if len(path) > 0:
-                    return path
+                while cur_node.parent is not None:
+                    neighbor = cur_node.neighbor
+                    steps.append(Step(neighbor.dir, neighbor.data))
+                    cur_node = cur_node.parent
+                if len(steps) > 0:
+                    return Path(steps, item_hash)
 
     @staticmethod
-    def merklestep(prev_hash, side):
-        if side[0] is 0:
-            return sha(prev_hash, side[1])
-        else:
-            return sha(side[1], prev_hash)
+    def verify_path(root_hash, path):
+        return MerkleTree.get_root_hash(path) == root_hash
+        
+    @staticmethod
+    def get_root_hash(path):
+        branch_hash = path.leaf_hash
+        for step in path.steps:
+            branch_hash = sha(branch_hash, step.hash) if (step.dir is Dir.left) else sha(step.hash, branch_hash)
+        return branch_hash
+    
+    @staticmethod
+    def __append_left(node_list, node):
+        node_list.append(node)
+    
+    @staticmethod
+    def __append_right(node_list, node):
+        neighbor = node_list[len(node_list) - 1]
+        node.neighbor = neighbor
+        node_list.append(node)
+        neighbor.neighbor = node
 
-
-def sha(val1, val2=None):
-    if val2 is None:
-        return hashlib.sha3_256(val1.encode('utf-8')).hexdigest()
-    return hashlib.sha3_256(val1.encode('utf-8') + val2.encode('utf-8')).hexdigest()
+def sha(*values):
+    return hashlib.sha3_256(reduce(lambda v1, v2: v1 + v2, values).encode('utf-8')).hexdigest()
--- test/test_merkletree.py
+++ test/test_merkletree.py
@@ -1,38 +1,78 @@
 import unittest
-from merkletree import MerkleTree, sha
-
+from merkletree import *
 
 class TestMerkleTree(unittest.TestCase):
 
-    def test_validate(self):
+    def test_verify(self):
         mt = MerkleTree(["node1", "node2", "node3", "node4", "node5", "node6", "node7", "node8"])
-        path = [(1, sha("node1")),
-                (0, sha(sha("node3"), sha("node4"))),
-                (0, sha(sha(sha("node5"), sha("node6")), sha(sha("node7"), sha("node8"))))]
-
-        self.assertTrue(mt.validate("node2", path))
+        path = Path([Step(Dir.right, sha("node1")),
+                         Step(Dir.left, sha(sha("node3"), sha("node4"))),
+                         Step(Dir.left, sha(sha(sha("node5"), sha("node6")), sha(sha("node7"), sha("node8"))))], 
+                        sha("node2"))
+        self.assertTrue(MerkleTree.verify_path(mt.root_hash, path), 
+                        "MerkleTree.verify falsely don't approve correct branch")
+        
+    def test_verify_odd_tree(self):
+        mt = MerkleTree(["node1", "node2", "node3", "node4", "node5"])
+        path = Path([Step(Dir.right, sha("node1")),
+                         Step(Dir.left, sha(sha("node3"), sha("node4"))),
+                         Step(Dir.left, sha(sha(sha("node5"), sha("node5")), sha(sha("node5"), sha("node5"))))], 
+                        sha("node2"))
+        self.assertTrue(MerkleTree.verify_path(mt.root_hash, path), 
+                        "MerkleTree.verify falsely don't approve correct branch for odd tree")
+        
+    def test_verify_odd_element(self):
+        mt = MerkleTree(["node1", "node2", "node3", "node4", "node5"])
+        path = Path([Step(Dir.left, sha("node5")),
+                         Step(Dir.right, sha(sha("node5"), sha("node5"))),
+                         Step(Dir.right, sha(sha(sha("node1"), sha("node2")), sha(sha("node3"), sha("node4"))))], 
+                        sha("node5"))
+        self.assertTrue(MerkleTree.verify_path(mt.root_hash, path), 
+                        "MerkleTree.verify falsely don't approve correct branch for odd tree for last element")
 
     def test_get_path(self):
         mt = MerkleTree(["node1", "node2", "node3", "node4", "node5", "node6", "node7", "node8"])
         path = mt.get_path("node5")
 
-        self.assertFalse(mt.validate("node2", path))
-        self.assertTrue(mt.validate("node5", path))
+        self.assertTrue(MerkleTree.verify_path(mt.root_hash, path), "MerkleTree.get_path returns incorrect path for even tree")
+        
+    def test_get_path_no_element(self):
+        mt = MerkleTree(["node1", "node2", "node3", "node4"])
 
-    def test_get_path_odd(self):
-        mt = MerkleTree(["node1", "node2", "node3", "node4", "node5", "node6", "node7"])
-        path = mt.get_path("node7")
-
-        self.assertFalse(mt.validate("node3", path))
-        self.assertTrue(mt.validate("node7", path))
-
-    def test_get_path_odd_path(self):
+        self.assertIsNone(mt.get_path("node5"), "MerkleTree.get_path returns incorrect path for even tree")
+        
+    def test_get_path_odd_tree(self):
         mt = MerkleTree(["node1", "node2", "node3", "node4", "node5"])
+        branch = mt.get_path("node3")
+        
+        self.assertTrue(MerkleTree.verify_path(mt.root_hash, branch), "MerkleTree.get_path returns incorrect path for odd tree") 
+        
+    def test_get_path_odd_element(self):
+        mt = MerkleTree(["node1", "node2", "node3", "node4", "node5"])
+        branch = mt.get_path("node5")
+        
+        self.assertTrue(MerkleTree.verify_path(mt.root_hash, branch), 
+                        "MerkleTree.get_path returns incorrect path for odd tree for the last element")
+        
+    def test_get_path_big_odd_tree(self):
+        mt = MerkleTree(["node1", "node2", "node3", "node4", "node5", "node6", "node7", 
+                         "node8", "node9", "node10", "node11", "node12", "node13"])
+
+        self.assertTrue(MerkleTree.verify_path(mt.root_hash, mt.get_path("node1")), 
+                        "MerkleTree.get_path or MerkleTree.verify works incorrectly for the first element")
+        self.assertTrue(MerkleTree.verify_path(mt.root_hash, mt.get_path("node9")),
+                        "MerkleTree.get_path or MerkleTree.verify works incorrectly for the element in the middle")
+        self.assertTrue(MerkleTree.verify_path(mt.root_hash, mt.get_path("node13")), 
+                        "MerkleTree.get_path or MerkleTree.verify works incorrectly for the last element")
+        
+    def test_get_path_fake_tree(self):
+        mt = MerkleTree(["node1", "node2", "node3", "node4", "node5", "node6", "node7", "node8"])
         path = mt.get_path("node5")
+        fake_mt = MerkleTree(["node1", "node2", "node3", "node4", "fake_node", "node6", "node7", "node8"])
+        fake_path = fake_mt.get_path("fake_node")
 
-        self.assertFalse(mt.validate("node3", path))
-        self.assertTrue(mt.validate("node5", path))
-
+        self.assertFalse(MerkleTree.verify_path(mt.root_hash, fake_path),
+                         "Merkle tree with counterfeit node incorrectly verified")
 
 if __name__ == '__main__':
     unittest.main()
